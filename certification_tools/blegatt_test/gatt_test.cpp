e5fcfa9309c738657179668269bb4be34ad28930
Deyao Ren <deyaoren@google.com>
2022-10-27 04:12:28 +0000

    Cherry-pick I1c8930d454fe595e39e45863f2d394eab85bb009 for UP1A.221031.001
    Complete acl_state_changed_callback and bt_callbacks_t signatures
    
    Bug: 255889707
    Change-Id: I85d1a13e900b7f5a79fcf4f44d4fa44a4d7232c3
    (cherry picked from commit 51b4938612dd6ada84b35a1ff1fa7f5afc3f8701)

--- certification_tools/blegatt_test/gatt_test.cpp
+++ certification_tools/blegatt_test/gatt_test.cpp
@@ -2012,7 +2012,7 @@ static void bond_state_changed_cb(bt_status_t status, RawAddress* remote_bd_addr
     g_PairState = state;
 }
 
-static void acl_state_changed(bt_status_t status, RawAddress* remote_bd_addr, bt_acl_state_t state, int transport_link_type, bt_hci_error_code_t hci_reason)
+static void acl_state_changed(bt_status_t status, RawAddress* remote_bd_addr, bt_acl_state_t state, int transport_link_type, bt_hci_error_code_t hci_reason, bt_conn_direction_t direction)
 {
     printf("acl_state_changed : remote_bd_addr=%02x:%02x:%02x:%02x:%02x:%02x, acl status=%s \n",
     remote_bd_addr->address[0], remote_bd_addr->address[1], remote_bd_addr->address[2],
@@ -2089,7 +2089,8 @@ static bt_callbacks_t bt_callbacks = {
     NULL, /* link_quality_report_cb */
     NULL, /* generate_local_oob_data_cb */
     NULL, /* switch_buffer_size_cb */
-    NULL /* switch_codec_cb */
+    NULL, /* switch_codec_cb */
+    NULL
 };
 
 static bt_os_callouts_t bt_os_callbacks = {
--- certification_tools/l2test_ertm/l2test_ertm.cpp
+++ certification_tools/l2test_ertm/l2test_ertm.cpp
@@ -593,7 +593,7 @@ static void bond_state_changed_cb(bt_status_t status, RawAddress *remote_bd_addr
     g_PairState = state;
 }
 
-static void acl_state_changed(bt_status_t status, RawAddress *remote_bd_addr, bt_acl_state_t state, int transport_link_type, bt_hci_error_code_t hci_reason)
+static void acl_state_changed(bt_status_t status, RawAddress *remote_bd_addr, bt_acl_state_t state, int transport_link_type, bt_hci_error_code_t hci_reason, bt_conn_direction_t direction)
 {
 }
 
@@ -623,7 +623,8 @@ static bt_callbacks_t bt_callbacks = {
     NULL, /* link_quality_report_cb */
     NULL,  /* generate_local_oob_data_cb */
     NULL, /* switch_buffer_size_cb */
-    NULL /* switch_codec_cb */
+    NULL, /* switch_codec_cb */
+    NULL
 };
 
 static bool set_wake_alarm(uint64_t delay_millis, bool should_wake, alarm_cb cb, void *data) {
